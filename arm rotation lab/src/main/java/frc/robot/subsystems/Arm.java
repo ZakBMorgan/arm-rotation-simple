7cc65b456c38eb787036bed90dcef6a323cdf298
ZakBMorgan <114784064+ZakBMorgan@users.noreply.github.com>
2023-10-13 17:56:15 -0700

    commented

--- arm rotation lab/src/main/java/frc/robot/subsystems/Arm.java
+++ arm rotation lab/src/main/java/frc/robot/subsystems/Arm.java
@@ -20,12 +20,15 @@ public class Arm extends SubsystemBase {
     Constants.PIDConstants.armPID_I,
     Constants.PIDConstants.armPID_D);
 
-  //create Talon objects, for rotate and extension
+  //create Talon objects, for rotation and extension
+  //import TalonSRX library by finding the 3rd party vendor link on WPILib website
   public static TalonSRX rotateTalon = new TalonSRX(Constants.OperatorConstants.armRotation);
   public static TalonSRX extensionTalon = new TalonSRX(Constants.OperatorConstants.armExtension);
   /** Creates a new Arm. */
   public Arm() {}
 
+  //setRotation, gets sine of angle and multiply by armHoldingVoltage in Constants subsystem
+  //divide by 12.0 because 12 volts is the ideal battery size
   public void setRot(double power) {
     rotateTalon.set(ControlMode.PercentOutput, power+(Math.sin(getAngle()))*(Constants.Measurements.armHoldingVoltage/12.0));
   }
@@ -34,14 +37,23 @@ public class Arm extends SubsystemBase {
     extensionTalon.set(ControlMode.PercentOutput, power);
   }
 
+  
   public double getAngle() {
     return (rotateTalon.getSelectedSensorPosition(0)/4096*360);
   }
 
   public void resetEncoders() {
     rotateTalon.setSelectedSensorPosition(0, 0, 10);
+    extensionTalon.setSelectedSensorPosition(0, 0, 10);
   }
 
+  public boolean fullyExtended() {
+    return extensionTalon.isFwdLimitSwitchClosed() == 0.0;
+  }
+
+  public boolean fullyRetracted() {
+    return extensionTalon.isRevLimitSwitchClosed() == 0.0;
+  }
 
 
   @Override
@@ -49,8 +61,13 @@ public class Arm extends SubsystemBase {
     // This method will be called once per scheduler run
     setRot(RobotContainer.getJoy1().getY());
     
-    if (RobotContainer.getJoy1().getPOV() == 0) {
-      extensionTalon.set();
+    if (RobotContainer.getJoy1().getPOV() == 180 && !fullyRetracted()) {
+      setExt(-0.5);
     }
+
+    if (RobotContainer.getJoy1().getPOV() == 0 && !fullyExtended()) {
+      setExt(0.5);
+    }
+
   }
 }
